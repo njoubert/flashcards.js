9f054e87daf1fd59f521707ba8af76f68cd8d342
Niels Joubert <njoubert@gmail.com>
2011-11-09 21:32:53 -0800

    ok i think i settled on a pattern

--- flashcards.js
+++ flashcards.js
@@ -8,14 +8,49 @@
 
 (function(global){ 
   var flashcards = function(element) {
-    
-    return {
-      next_card: function() {
-        
-      }      
-    }
-    
+    return new Flashcard(element);
   }
+
+  var Flashcard = function(element) {
+    rootel = element;
+    extractor = null;
+    itemA     = null;
+    itemB     = null;
+    shuffle   = true;
+    this.init();
+  }
+  
+  Flashcard.prototype.init = function() {
+    if (extractor === null || itemA === null || itemB === null)
+      return this;
+      
+    console.log("Initializing flashcards on " + rootel.getAttribute("id"))
+
+    return this;
+  }
+  
+  Flashcard.prototype.data = function(fn) {
+    extractor = fn;
+    this.init();
+    return this;
+  }
+  
+  Flashcard.prototype.itemA = function(fn) {
+    itemA = fn;
+    return this.init();
+  }
+
+  Flashcard.prototype.itemB = function(fn) {
+    itemB = fn;
+    return this.init();
+  }
+
+  Flashcard.prototype.shuffle = function(b) {
+    shuffle = b;
+    return this.init();
+  }
+  
+  
   
   flashcards.VERSION = '0.0.1';
   
